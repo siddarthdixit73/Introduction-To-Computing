0517793a480a8f676dc3180414513a3a94e2d4c2
siddarthdixit73 <97831696+siddarthdixit73@users.noreply.github.com>
2022-02-13 22:21:16 +0530

    Add files via upload

---
+++ Output_file.py
@@ -0,0 +1,78 @@
+The solution of Question 1 is:
+
+Enter the string: My name is Siddarth Dixit siddarth is my name
+The occurence(s) of:
+        "my" is/are 2
+        "name" is/are 2
+        "is" is/are 2
+        "siddarth" is/are 2
+        "dixit" is/are 1
+
+The solution of Question 2 is:
+
+Day - 03
+Month - 02
+Year - 2021
+Next Date is: 4/2/2021
+
+The solution of Question 3 is:
+
+Enter the list: [4,5,6]
+Output: [(4, 16), (5, 25), (6, 36)]
+
+The solution of Question 4 is:
+
+Enter the grade point of the student: 7.8
+Your Grade is 'B' and Good Performance
+
+The solution of Question 5 is:
+
+ABCDEFGHIJK
+ ABCDEFGHI
+  ABCDEFG
+   ABCDE
+    ABC
+     A
+
+The solution of Question 6 is:
+
+Enter the name of the student: Siddarth Dixit
+Enter the SID of Siddarth Dixit: 21105109
+
+You have entered 1 value(s) till now
+Do you want to enter more data? y
+Enter the name of the student: ABC
+Enter the SID of ABC: 123
+
+You have entered 2 value(s) till now
+Do you want to enter more data? n
+
+a. Student Details:
+The SID of Siddarth Dixit is 21105109
+The SID of ABC is 123
+
+b. Student Details (sorted with respect to names):
+The SID of ABC is 123
+The SID of Siddarth Dixit is 21105109
+
+c. Student Details (sorted with respect to SIDs):
+The SID of ABC is 123
+The SID of Siddarth Dixit is 21105109
+
+d. Enter the SID of the student: 21105109
+The name of student whose SID is 21105109 is Siddarth Dixit
+
+The solution of Question 7 is:
+
+Enter the no. of terms of the Fibonacci sequence: 5
+The Fibonacci sequence is as follows:
+0 1 1 2 3 
+The average of the terms of Fibonacci sequence upto 5 terms is 1.40
+
+The solution of Question 8 is:
+
+a. Set of all elements that are in Set1 and Set2 but not both: {1, 3, 5, 6, 8}
+b. Set of all elements that are in only one of the three sets Set1, Set2 and Set3: {17, 3, 6, 8, 9, 13}
+c. Set of elements that are in exactly two of the sets Set1, Set2 and Set3: {1, 2, 4, 5}
+d. Set of all integers in the range 1 to 10 that are not in Set1: {6, 7, 8, 9, 10}
+e. Set of all integers in the range 1 to 10 that are not in Set1, Set2 and Set3: {10, 7}
