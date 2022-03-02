a295920391b2858c72173d475f8e592c1177c098
siddarthdixit73 <97831696+siddarthdixit73@users.noreply.github.com>
2022-03-02 11:26:55 +0530

    Add files via upload

---
+++ output 4.py
@@ -0,0 +1,81 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============== RESTART: D:\PEC\PYTHON\assignment 4\assignment 4.py =============
+----------------------------------ASSIGNMENT 4-----------------------------
+
+Question #1
+SOL 1. :- Solution the problem of tower of Hanoi with three disks.
+
+Move Disk 1 from A to C
+Move Disk 2 from A to B
+Move Disk 1 from C to B
+Move Disk 3 from A to C
+Move Disk 1 from B to A
+Move Disk 2 from B to C
+Move Disk 1 from A to C
+
+Question #2
+ 
+Enter the number of rows in Pascal's Triangle: 5
+
+Using recursions:
+
+        1   
+      1   1   
+    1   2   1   
+  1   3   3   1   
+1   4   6   4   1   
+
+Using loops:
+
+        1   
+      1   1   
+    1   2   1   
+  1   3   3   1   
+1   4   6   4   1   
+
+Question #3
+ 
+ 
+Enter the first integer: 1
+Enter the first integer: 2
+Remainder is:  1
+Quotient is:  0
+One value is zero
+{5}
+{5, 6}
+{5, 6}
+{5, 6, 7}
+{5, 6, 7}
+{5, 6, 7}
+Immutable set:  frozenset({5, 6, 7})
+Largest value in the set:  7
+Hash value:  7
+
+Question #4
+ 
+ 
+Object created
+The name of the student it Siddarth Dixit and SID is 21105109.
+Object destroyed
+
+QUESTION #5
+ 
+ 
+a. The updated salary of the employee Mehak is 70000
+b. Employee Viren record deleted
+ 
+QUESTION #6
+ 
+ 
+Enter the first word: 3
+
+Enter a new meaningful word to test your friendship: 3
+
+Does the word makes sense?(y or n)
+y
+The friends pass the friendship test!!
+
+
+
